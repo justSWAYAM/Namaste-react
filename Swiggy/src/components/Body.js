6bf9902e3a502a7ad71d9b25524d06ce0a3508a1
justSWAYAM <just.swayammishra@gmail.com>
2024-06-07 03:06:50 +0530

    Ep5-done

--- Swiggy/src/components/Body.js
+++ Swiggy/src/components/Body.js
@@ -1,14 +1,34 @@
-import resList from "../utils/mockData.js";
 
 import RestaurantCard from "./RestaurantCard.js";
+import { useState } from "react";
+import resList from "../utils/mockData.js";
 
 const Body =()=>{
+
+    
+const [ListOfRestaurant, SetRestaurantList] = useState(resList);
+
     return (
-    <div className="body">
-    <div className="Search">Search</div>
+    <div className="body"> 
+
+    <div className="filter">
+
+    <button className="Filter-btn-Top"
+    onClick={() => {
+        
+       const FilteredList = ListOfRestaurant.filter( (res) => res.info.avgRating > 4.2);
+
+    console.log(FilteredList);
+    
+   SetRestaurantList(FilteredList);
+}}
+
+    > Top Rated Restaurant </button>
+    </div>
+
     <div className="Res-container">
     {
-        resList.map(restaurant => (<RestaurantCard key ={restaurant.info.id} resData={restaurant} />))
+        ListOfRestaurant.map(restaurant => (<RestaurantCard key ={restaurant.info.id} resData={restaurant} />))
     }
     </div>
     </div>
