6739286b46e3c89502cdb80270e63d27f1cec447
justSWAYAM <just.swayammishra@gmail.com>
2024-06-29 19:27:59 +0530

    dist

---
+++ Swiggy/dist/Grocery.74d67e99.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire29b1;(0,e.register)("loR3J",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>o,set:void 0,enumerable:!0,configurable:!0});var l=e("ayMG0"),o=()=>(0,l.jsx)("div",{children:(0,l.jsx)("h1",{children:"Grocery"})})});
+//# sourceMappingURL=Grocery.74d67e99.js.map
